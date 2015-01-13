0acb26339e28533d53704846c1368b3ca7b3ce42
Maxwell Vandervelde <Max@MaxVandervelde.com>
2015-01-12 21:09:06 -0600

    Updating Dependencies.
    Jackson was having issues with deserialization. Updated and changed to
    latest syntax.

--- src/main/java/com/animedetour/sched/api/model/Event.java
+++ src/main/java/com/animedetour/sched/api/model/Event.java
@@ -19,78 +19,93 @@ import java.io.Serializable;
  *
  * @author Maxwell Vandervelde (Max@MaxVandervelde.com)
  */
+@SuppressWarnings("UnusedDeclaration")
 public class Event implements Serializable
 {
     /**
      * Globally Unique ID for the panel.
      */
     @DatabaseField(id = true)
+    @JsonProperty("id")
     private String id;
 
     /**
      * A key specific to this panel.
      */
     @DatabaseField
+    @JsonProperty("event_key")
     private String eventKey;
 
     /**
      * The Name of the panel.
      */
     @DatabaseField
+    @JsonProperty("name")
     private String name;
 
     /**
      * The start time of the panel.
      */
     @DatabaseField(index = true, dataType = DataType.DATE_TIME)
+    @JsonProperty("event_start")
+    @JsonDeserialize(using = PanelDateDeserializer.class)
     private DateTime start;
 
     /**
      * End time of the panel.
      */
     @DatabaseField(dataType = DataType.DATE_TIME)
+    @JsonProperty("event_end")
+    @JsonDeserialize(using = PanelDateDeserializer.class)
     private DateTime end;
 
     /**
      * The Type specified for the event. E.g. 'Programming' or 'Room Parties'.
      */
     @DatabaseField(index = true)
+    @JsonProperty("event_type")
     private String eventType;
 
     /**
      * An optional subtype for the event. E.g. 'Panel' or 'Game'.
      */
     @DatabaseField
+    @JsonProperty("event_subtype")
     private String eventSubType;
 
     /**
      * The detailed description of the event panel.
      */
     @DatabaseField
+    @JsonProperty("description")
     private String description;
 
     /**
      * The name of the venue location where the panel is located.
      */
     @DatabaseField
+    @JsonProperty("venue")
     private String venue;
 
     /**
      * A unique Identifier for the venue location where the panel is located.
      */
     @DatabaseField(index = true)
+    @JsonProperty("venue_id")
     private String venueId;
 
     /**
      * A list of official speakers running the event.
      */
     @DatabaseField
+    @JsonProperty("speakers")
     private String speakers;
 
     /**
      * A URL to a relevant banner image to the event.
      */
     @DatabaseField
+    @JsonProperty("media_url")
     private String mediaUrl;
 
     /**
@@ -110,7 +125,6 @@ public class Event implements Serializable
     /**
      * @param id Globally Unique ID for the panel
      */
-    @JsonProperty("id")
     public void setId(String id)
     {
         this.id = id;
@@ -127,7 +141,6 @@ public class Event implements Serializable
     /**
      * @param eventKey A key specific to this panel
      */
-    @JsonProperty("event_key")
     public void setEventKey(String eventKey)
     {
         this.eventKey = eventKey;
@@ -144,7 +157,6 @@ public class Event implements Serializable
     /**
      * @param name The Name of the panel
      */
-    @JsonProperty("name")
     public void setName(String name)
     {
         this.name = name;
@@ -165,8 +177,6 @@ public class Event implements Serializable
      *
      * @param start The start time of the panel
      */
-    @JsonProperty("event_start")
-    @JsonDeserialize(using = PanelDateDeserializer.class)
     public void setStartDateTime(DateTime start)
     {
         this.start = start;
@@ -187,8 +197,6 @@ public class Event implements Serializable
      *
      * @param end The end time of the panel
      */
-    @JsonProperty("event_end")
-    @JsonDeserialize(using = PanelDateDeserializer.class)
     public void setEndDateTime(DateTime end)
     {
         this.end = end;
@@ -205,7 +213,6 @@ public class Event implements Serializable
     /**
      * @param eventType The Type specified for the event. E.g. 'Programming' or 'Room Parties'
      */
-    @JsonProperty("event_type")
     public void setEventType(String eventType)
     {
         this.eventType = eventType;
@@ -222,7 +229,6 @@ public class Event implements Serializable
     /**
      * @param eventSubType An optional subtype for the event. E.g. 'Panel' or 'Game'
      */
-    @JsonProperty("event_subtype")
     public void setEventSubType(String eventSubType)
     {
         this.eventSubType = eventSubType;
@@ -239,7 +245,6 @@ public class Event implements Serializable
     /**
      * @param description The detailed description of the event panel
      */
-    @JsonProperty("description")
     public void setDescription(String description)
     {
         this.description = description;
@@ -256,7 +261,6 @@ public class Event implements Serializable
     /**
      * @param venue The name of the venue location where the panel is located
      */
-    @JsonProperty("venue")
     public void setVenue(String venue)
     {
         this.venue = venue;
@@ -273,7 +277,6 @@ public class Event implements Serializable
     /**
      * @param venueId A unique Identifier for the venue location where the panel is located
      */
-    @JsonProperty("venue_id")
     public void setVenueId(String venueId)
     {
         this.venueId = venueId;
@@ -290,7 +293,6 @@ public class Event implements Serializable
     /**
      * @param speakers A list of official speakers running the event
      */
-    @JsonProperty("speakers")
     public void setSpeakers(String speakers)
     {
         this.speakers = speakers;
@@ -307,7 +309,6 @@ public class Event implements Serializable
     /**
      * @param url A URL to a relevant banner image for the event
      */
-    @JsonProperty("media_url")
     public void setMediaUrl(String url)
     {
         url = url.replaceAll(" ", "%20");
@@ -322,5 +323,25 @@ public class Event implements Serializable
     public void setFetched(DateTime fetched)
     {
         this.fetched = fetched;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Event{" +
+            "id='" + id + '\'' +
+            ", eventKey='" + eventKey + '\'' +
+            ", name='" + name + '\'' +
+            ", start=" + start +
+            ", end=" + end +
+            ", eventType='" + eventType + '\'' +
+            ", eventSubType='" + eventSubType + '\'' +
+            ", description='" + description + '\'' +
+            ", venue='" + venue + '\'' +
+            ", venueId='" + venueId + '\'' +
+            ", speakers='" + speakers + '\'' +
+            ", mediaUrl='" + mediaUrl + '\'' +
+            ", fetched=" + fetched +
+            '}';
     }
 }
