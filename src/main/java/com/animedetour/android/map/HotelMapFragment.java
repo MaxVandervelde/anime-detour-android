561d3c324e84daa47f7fcee7083164c7f6ff01bd
Maxwell Vandervelde <Max@MaxVandervelde.com>
2015-02-16 15:37:14 -0600

    Add second and 22nd floors along with a switch to control them.

--- src/main/java/com/animedetour/android/map/HotelMapFragment.java
+++ src/main/java/com/animedetour/android/map/HotelMapFragment.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Anime Detour Android application
  *
- * Copyright (c) 2014 Anime Twin Cities, Inc.
+ * Copyright (c) 2014-2015 Anime Twin Cities, Inc.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,40 +12,69 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.animedetour.android.R;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.GroundOverlayOptions;
-import com.google.android.gms.maps.model.Marker;
-
-import java.util.ArrayList;
 
 import static com.animedetour.android.map.HotelMapPoints.*;
 
 /**
- * Google Maps display with Hotel points overlayed.
+ * Google Maps display with Hotel floor plans overlayed.
  *
  * @author Maxwell Vandervelde (Max@MaxVandervelde.com)
  */
 final public class HotelMapFragment extends SupportMapFragment
 {
-    private ArrayList<Marker> markers = new ArrayList<>();
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View view = super.onCreateView(inflater, container, savedInstanceState);
-        this.resetMap();
+        View mapView = super.onCreateView(inflater, container, savedInstanceState);
+        View controlView = inflater.inflate(R.layout.map_controls, container, false);
+        FrameLayout composite = new FrameLayout(this.getActivity());
+        composite.addView(mapView);
+        composite.addView(controlView);
 
-        return view;
+        return composite;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        ButterKnife.inject(this, view);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        this.resetMap();
+        this.centerMap();
+        this.showFirstFloor();
     }
 
     /**
-     * Clear the map and re-draw the base hotel.
+     * Center the map camera on the hotel and zoom appropriately.
+     */
+    private void centerMap()
+    {
+        GoogleMap map = this.getMap();
+        CameraUpdate camera = CameraUpdateFactory.newCameraPosition(
+            new CameraPosition.Builder().target(HOTEL_CENTER).zoom(18F).tilt(0).bearing(180).build()
+        );
+        map.moveCamera(camera);
+    }
+
+    /**
+     * Reset any overlays from the map so we can draw new floors.
      */
     private void resetMap()
     {
@@ -56,17 +85,36 @@ final public class HotelMapFragment extends SupportMapFragment
         map.getUiSettings().setTiltGesturesEnabled(false);
         map.getUiSettings().setRotateGesturesEnabled(false);
         map.getUiSettings().setCompassEnabled(false);
+        map.getUiSettings().setZoomControlsEnabled(false);
+    }
 
-        CameraUpdate camera = CameraUpdateFactory.newCameraPosition(
-            new CameraPosition.Builder().target(HOTEL_CENTER).zoom(18F).tilt(0).bearing(180).build()
-        );
-        map.moveCamera(camera);
+    /**
+     * Clear the map and draw the first floor map.
+     */
+    @OnClick(R.id.map_control_first_floor)
+    public void showFirstFloor()
+    {
+        this.resetMap();
+        this.getMap().addGroundOverlay(HotelMapPoints.getFirstFloorOverlay());
+    }
 
-        map.addGroundOverlay(
-            new GroundOverlayOptions()
-                .image(BitmapDescriptorFactory.fromResource(R.drawable.map_floor_1))
-                .position(HOTEL_CENTER, 160)
-                .bearing(180)
-        );
+    /**
+     * Clear the map and draw the second floor map.
+     */
+    @OnClick(R.id.map_control_second_floor)
+    public void showSecondFloor()
+    {
+        this.resetMap();
+        this.getMap().addGroundOverlay(HotelMapPoints.getSecondFloorOverlay());
+    }
+
+    /**
+     * Clear the map and draw the 22nd floor map.
+     */
+    @OnClick(R.id.map_control_22nd_floor)
+    public void show22ndFloor()
+    {
+        this.resetMap();
+        this.getMap().addGroundOverlay(HotelMapPoints.get22ndFloorOverlay());
     }
 }
--- src/main/java/com/animedetour/android/map/HotelMapPoints.java
+++ src/main/java/com/animedetour/android/map/HotelMapPoints.java
@@ -1,13 +1,16 @@
 /*
  * This file is part of the Anime Detour Android application
  *
- * Copyright (c) 2014 Anime Twin Cities, Inc.
+ * Copyright (c) 2014-2015 Anime Twin Cities, Inc.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 package com.animedetour.android.map;
 
+import com.animedetour.android.R;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 
 /**
@@ -18,10 +21,51 @@ import com.google.android.gms.maps.model.LatLng;
  * there's currently no good API for that. Since the building doesn't change
  * often, this should be fine.
  *
- * @todo This needs to be completed when we have more of the map finalized.
  * @author Maxwell Vandervelde (Max@MaxVandervelde.com)
  */
 final class HotelMapPoints
 {
-    final static LatLng HOTEL_CENTER = new LatLng(44.8619752, -93.3530438);
+    /**
+     * Center point of the hotel to base overlay images on and to focus the map on.
+     */
+    final public static LatLng HOTEL_CENTER = new LatLng(44.8619752, -93.3530438);
+
+    /**
+     * Positioning options for the first floor map image to overlay on the map.
+     */
+    public static GroundOverlayOptions getFirstFloorOverlay()
+    {
+        GroundOverlayOptions options = new GroundOverlayOptions();
+        options.image(BitmapDescriptorFactory.fromResource(R.drawable.map_floor_1));
+        options.position(HOTEL_CENTER, 160);
+        options.bearing(180);
+
+        return options;
+    }
+
+    /**
+     * Positioning options for the second floor map image to overlay on the map.
+     */
+    public static GroundOverlayOptions getSecondFloorOverlay()
+    {
+        GroundOverlayOptions options = new GroundOverlayOptions();
+        options.image(BitmapDescriptorFactory.fromResource(R.drawable.map_floor_2));
+        options.position(HOTEL_CENTER, 160);
+        options.bearing(180);
+
+        return options;
+    }
+
+    /**
+     * Positioning options for the 22nd floor map image to overlay on the map.
+     */
+    public static GroundOverlayOptions get22ndFloorOverlay()
+    {
+        GroundOverlayOptions options = new GroundOverlayOptions();
+        options.image(BitmapDescriptorFactory.fromResource(R.drawable.map_floor_22));
+        options.position(HOTEL_CENTER, 160);
+        options.bearing(180);
+
+        return options;
+    }
 }
