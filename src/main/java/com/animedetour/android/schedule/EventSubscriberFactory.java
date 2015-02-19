57528af080301125bea0d33f4b88faaf121adc82
Maxwell Vandervelde <Max@MaxVandervelde.com>
2015-02-18 21:10:21 -0600

    Fix scrollbar sync issues.
    Updated the event subscriber to manage the scrollbar position on event updates
    instead of the activity. Created a factory for the subscriber. Updated the
    subscriber to properly get and set the vertical position of the list with
    the recyclerview.
    
    The event subscriber temporarily contains an unchecked cast for the layout
    manager here in order to make this work for now. This should be refactored,
    however, following commits will likely revert this back to a listview
    making that change defunct, and will hinder the change back to a
    listview.

---
+++ src/main/java/com/animedetour/android/schedule/EventSubscriberFactory.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the Anime Detour Android application
+ *
+ * Copyright (c) 2015 Anime Twin Cities, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+package com.animedetour.android.schedule;
+
+import android.view.View;
+import com.animedetour.api.sched.api.model.Event;
+import com.inkapplications.android.widget.recyclerview.SimpleRecyclerView;
+import org.apache.commons.logging.Log;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Creates new Event Update subscriber factories at runtime.
+ *
+ * Since the subscribers need views that are not accessible until runtime, this
+ * class will create and inject them for us.
+ *
+ * @author Maxwell Vandervelde (Max@MaxVandervelde.com)
+ */
+@Singleton
+public class EventSubscriberFactory
+{
+    final private Log logger;
+
+    @Inject
+    public EventSubscriberFactory(Log logger)
+    {
+        this.logger = logger;
+    }
+
+    public EventUpdateSubscriber create(
+        SimpleRecyclerView<PanelView, Event> panelList,
+        View emptyView
+    ) {
+        return new EventUpdateSubscriber(panelList, emptyView, this.logger);
+    }
+}
