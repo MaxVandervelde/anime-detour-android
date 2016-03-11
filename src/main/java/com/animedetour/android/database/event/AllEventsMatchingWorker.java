0b51a87ccf33eaf3644f47c34bc4b9a3f0c0fd71
Maxwell Vandervelde <Max@MaxVandervelde.com>
2016-03-10 22:30:31 -0600

    Update Search logic to include tags and hosts.
    added like matching to the tags and hosts parameters. Added indexes to
    the name and category events. Trimmed the input criteria for better
    matching.

--- src/main/java/com/animedetour/android/database/event/AllEventsMatchingWorker.java
+++ src/main/java/com/animedetour/android/database/event/AllEventsMatchingWorker.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Anime Detour Android application
  *
- * Copyright (c) 2015 Anime Twin Cities, Inc.
+ * Copyright (c) 2015-2016 Anime Twin Cities, Inc.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -26,10 +26,9 @@ import java.util.List;
  * the user.
  *
  * Events are matched *roughly* by name, and will match if the exact query
- * matches the event type, to allow for filtering.
+ * matches the event category, tags or hosts, to allow for filtering.
  *
- * Events will be any that *start* on the specified date and ordered by start
- * time.
+ * Events will be ordered by start time, then name.
  *
  * @author Maxwell Vandervelde (Max@MaxVandervelde.com)
  */
@@ -56,8 +55,13 @@ public class AllEventsMatchingWorker extends SyncEventsWorker
         QueryBuilder<Event, String> builder = this.localAccess.queryBuilder();
         builder.orderBy("start", true);
         builder.orderBy("name", true);
-        builder.where().like("name","%" + this.criteria + "%")
-                .or().eq("category", this.criteria);
+
+        String criteria = this.criteria.trim();
+
+        builder.where().like("name","%" + criteria + "%")
+            .or().eq("category", criteria)
+            .or().like("tags", "%" + criteria + "%")
+            .or().like("hosts", "%" + criteria + "%");
 
         PreparedQuery<Event> query = builder.prepare();
         List<Event> result = this.localAccess.query(query);
